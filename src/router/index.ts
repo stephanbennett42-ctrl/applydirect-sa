dcda47b36990c327f570a7e1db88ed44e2e72ce7
Stephan Bennett <stephanbennett42@gmail.com>
2026-08-26 10:06:52 +0200

    Register /institutions route and default redirect in Vue Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import InstitutionsView from '../views/InstitutionsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/institutions'
+    },
+    {
+      path: '/institutions',
+      name: 'institutions',
+      component: InstitutionsView
+    }
+  ]
 })
 
 export default router
