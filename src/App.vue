71e90516c76f3cc89a50335aa080314cee236e73
Stephan Bennett <stephanbennett42@gmail.com>
2026-08-26 09:56:15 +0200

    Update App.vue with navigation navbar and RouterView placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+      <div class="container">
+        <RouterLink class="navbar-brand fw-bold" to="/institutions">ApplyDirect SA</RouterLink>
+        <div class="navbar-nav">
+          <RouterLink class="nav-link" to="/institutions">Institutions</RouterLink>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
