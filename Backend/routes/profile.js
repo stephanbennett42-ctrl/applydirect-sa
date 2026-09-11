018627eb281a8d36f788d4b6d6f422babfc74aef
Owam Gobinca <owamgobingca@gmail.com>
2026-09-11 15:36:58 +0200

    Added CRUD fuctions

--- Backend/routes/profile.js
+++ Backend/routes/profile.js
@@ -1,11 +1,24 @@
 const express = require('express');
 
 const {
-    getPortfolio
+    getPortfolio,
+    createPortfolio,
+    updatePortfolio,
+    deletePortfolio
 } = require('../controllers/profile');
 
 const router = express.Router();
 
+// READ
 router.get('/:profile_id', getPortfolio);
+
+// CREATE
+router.post('/', createPortfolio);
+
+// UPDATE
+router.put('/:profile_id', updatePortfolio);
+
+// DELETE
+router.delete('/:profile_id', deletePortfolio);
 
 module.exports = router;
