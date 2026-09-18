a6938aac54117ff0e7d8713b82137812dcdd592e
Owam Gobinca <owamgobingca@gmail.com>
2026-09-18 09:17:26 +0200

    Connected successfully to backend

--- frontend/src/views/contact.vue
+++ frontend/src/views/contact.vue
@@ -149,7 +149,7 @@ export default {
       try {
         console.log('Sending contact form:', this.form);
 
-        const response = await fetch('http://localhost:3000/api/contact', {
+        const response = await fetch('http://127.0.0.1:3000/api/contact', {
           method: 'POST',
 
           headers: {
